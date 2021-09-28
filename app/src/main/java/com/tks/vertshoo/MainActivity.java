97fcf3ac84b164d33ce5bbfa8ef9b6faeb63b1a4
aaaaa <aaaaa>
2021-09-28 09:02:28 +0900

    コミット漏れ分を追加

--- app/src/main/java/com/tks/vertshoo/MainActivity.java
+++ app/src/main/java/com/tks/vertshoo/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.eaglesakura.lib.android.game.activity.GL11GameActivityBase;
+import com.eaglesakura.lib.android.game.loop.GameLoopManagerBase;
+import com.eaglesakura.lib.android.game.math.Vector2;
+
+public class MainActivity extends GL11GameActivityBase {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected GameLoopManagerBase createGameLoopManager() {
+        return new FoxOne(this, this);
+    }
+
+    @Override
+    protected Vector2 getVirtualDisplaySize() {
+        return new Vector2(Define.VIRTUAL_DISPLAY_WIDTH, Define.VIRTUAL_DISPLAY_HEIGHT);
     }
 }
